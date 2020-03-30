c2ccde051d9db551869f6502725eb48a1bc5d983
yzk <yzk@rupeng.com>
2020-03-30 17:01:58 +0800

    init

---
+++ Zh_HansIdentityErrorDescriber.cs
@@ -0,0 +1,202 @@
+﻿namespace Microsoft.AspNetCore.Identity
+{
+    public class Zh_HansIdentityErrorDescriber: IdentityErrorDescriber
+    {
+        public override IdentityError ConcurrencyFailure()
+        {
+            return new IdentityError {
+                Code = nameof(ConcurrencyFailure),
+                Description = "乐观并发失败，对象已被修改"
+            };
+        }
+
+        public override IdentityError DefaultError()
+        {
+            return new IdentityError
+            {
+                Code = nameof(DefaultError),
+                Description= "发生了未知的故障"
+            };
+        }
+
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateEmail),
+                Description = $"电子邮件'{email}'已被占用"
+            };
+        }
+
+        public override IdentityError DuplicateRoleName(string role)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateRoleName),
+                Description = $"角色名 '{role}'已被占用"
+            };
+        }
+
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateUserName),
+                Description=$"用户名 '{userName}'已被占用"
+            };
+        }
+
+        public override IdentityError InvalidEmail(string email)
+        {
+            return new IdentityError
+            {
+                Code = nameof(InvalidEmail),
+                Description=$"邮箱{email}非法"
+            };
+        }
+
+        public override IdentityError InvalidRoleName(string role)
+        {
+            return new IdentityError
+            {
+                Code = nameof(InvalidRoleName),
+                Description = $"角色名{role}非法"
+            };
+        }
+
+        public override IdentityError InvalidToken()
+        {
+            return new IdentityError
+            {
+                Code = nameof(InvalidToken),
+                Description="非法的令牌"
+            };
+        }
+
+        public override IdentityError InvalidUserName(string userName)
+        {
+            return new IdentityError
+            {
+                Code = nameof(InvalidUserName),
+                Description=$"用户名'{userName}' 非法, 只能包含字母或者数字"
+            };
+        }
+
+        public override IdentityError LoginAlreadyAssociated()
+        {
+            return new IdentityError
+            {
+                Code = nameof(LoginAlreadyAssociated),
+                Description=$"具有此登录的用户已经存在"
+            };
+        }
+
+        public override IdentityError PasswordMismatch()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordMismatch),
+                Description="密码错误"
+            };
+        }
+
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresDigit),
+                Description= "密码必须至少有一位数字('0'-'9')"
+            };
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresLower),
+                Description= "密码必须至少有一个小写字母('a'-'z')"
+            };
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresNonAlphanumeric),
+                Description= "密码必须至少有一个非字母数字字符"
+            };
+        }
+
+        public override IdentityError PasswordRequiresUniqueChars(int uniqueChars)
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresUniqueChars),
+                Description=$"密码必须使用至少有{uniqueChars}个不同的字符"
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresUpper),
+                Description= "密码必须至少有一个大写字母('A'-'Z')"
+            };
+        }
+
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordTooShort),
+                Description=$"密码必须至少有{length}个字符"
+            };
+        }
+
+        public override IdentityError RecoveryCodeRedemptionFailed()
+        {
+            return new IdentityError
+            {
+                Code = nameof(RecoveryCodeRedemptionFailed),
+                Description= "恢复码赎回失败。"
+            };
+        }
+
+        public override IdentityError UserAlreadyHasPassword()
+        {
+            return new IdentityError
+            {
+                Code = nameof(UserAlreadyHasPassword),
+                Description= "用户已经设置过密码"
+            };
+        }
+
+        public override IdentityError UserAlreadyInRole(string role)
+        {
+            return new IdentityError
+            {
+                Code = nameof(UserAlreadyInRole),
+                Description=$"用户已经在角色{role}中"
+            };
+        }
+
+        public override IdentityError UserLockoutNotEnabled()
+        {
+            return new IdentityError
+            {
+                Code = nameof(UserLockoutNotEnabled),
+                Description= "未为此用户启用锁定"
+            };
+        }
+
+        public override IdentityError UserNotInRole(string role)
+        {
+            return new IdentityError
+            {
+                Code = nameof(UserNotInRole),
+                Description=$"用户不在角色{role}中"
+            };
+        }
+    }
+}
---
+++ Zh_HansIdentityErrorDescriberExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNetCore.Identity
+{
+    public static class Zh_HansIdentityErrorDescriberExtensions
+    {
+        public static IdentityBuilder AddZh_HansIdentityErrorDescriber(this IdentityBuilder builder)
+        {
+            return builder.AddErrorDescriber<Zh_HansIdentityErrorDescriber>();
+        }
+    }
+}
